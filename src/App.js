cafa3c1252e403acb0c30536c2fe4ee3137353c8
Katie Eells <katieeells@Katies-MBP.lan>
2022-06-21 21:56:25 -0400

    completed with bonus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
-    <div className="App">
+  const [gitHubName, setGitHubName] = useState('')     
+  const [gitHubURL, setGitHubURL] = useState('') 
+  const [gitHubImageURL, setGitHubImageURL] = useState('')                     
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/kleells')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+        setGitHubURL(data.html_url)
+        setGitHubImageURL(data.avatar_url)
+    })
+}, [])
+
+return (
+  <div className="App">
+    <div>
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubURL}><button>Link to GitHub profile</button></a>
     </div>
-  );
+    <div className="pt-5">
+      <img src={gitHubImageURL} alt='Github profile image' width='200' height='200'/>
+    </div>
+  </div>
+);
 }
 
-export default App;
+export default App
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// sets up the mock and adds the fetchMock -g variable and requires 'fetch' -g to call 'fetchMock' instead of a real fetch
+require('jest-fetch-mock').enableMocks()
+
